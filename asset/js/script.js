c6848190d4ab6fc58fed2b48ce0ae576bd456030
Steph-en <appahstephen9@gmail.com>
2021-09-28 00:28:31 +0000

    done with changes

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,13 +1,13 @@
 const smallCups = document.querySelectorAll(".cup-small");
-const liters = document.getElementById("#liters");
-const percentage = document.getElementById("#percentage");
-const remained = document.getElementById("#remained");
+const liters = document.getElementById("liters");
+const percentage = document.getElementById("percentage");
+const remained = document.getElementById("remained");
 
 updateBigCup();
 
 smallCups.forEach((cup, idx) => {
-	cup.addEventListener("click", () => highlightCups(idx));
-});
+	cup.addEventListener("click", () => highlightCups(idx))
+})
 
 function highlightCups(idx) {
 	if (idx === 7 && smallCups[idx].classList.contains("full")) idx--;
